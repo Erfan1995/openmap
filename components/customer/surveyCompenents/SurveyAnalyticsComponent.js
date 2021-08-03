9dbeb3c26371ea3497b8530d3509b10de9d35373
mbshaban <m.belal.shaban@gmail.com>
2021-08-03 14:36:11 +0430

    surveyanalytics error solved

--- components/customer/surveyCompenents/SurveyAnalyticsComponent.js
+++ components/customer/surveyCompenents/SurveyAnalyticsComponent.js
@@ -1,17 +1,16 @@
+import "survey-analytics/survey.analytics.css";
+import * as Survey from "survey-react";
+import { data, json } from "./analytics_data";
+import { useEffect } from "react";
 import { VisualizationPanel } from "survey-analytics";
-// import "survey-analytics/survey.analytics.css";
-// import * as Survey from "survey-react";
-// import { data, json } from "./analytics_data";
-// import { useEffect } from "react";
-// import $ from "jquery";
 
 const SurveyAnalyticsComponent = () => {
-    // let visPanel;
-    // useEffect(() => {
-    //     const survey = new Survey.SurveyModel(json);
-    //     visPanel = new VisualizationPanel(survey.getAllQuestions(), data);
-    //     visPanel.render(document.getElementById("summaryContainer"));
-    // })
+    let visPanel;
+    useEffect(() => {
+        const survey = new Survey.SurveyModel(json);
+        visPanel = new VisualizationPanel(survey.getAllQuestions(), data);
+        visPanel.render(document.getElementById("summaryContainer"));
+    })
     return (
         <div id="summaryContainer">
         </div>
