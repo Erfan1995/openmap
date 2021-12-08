611168bbb06c152dbd255d2efbd37c24c0650828
aliriza erfan <aliriza.erfan@gmail.com>
2021-12-08 21:36:11 +0430

    sdfsf'

--- components/client/widget/progressbar/Progressbar.js
+++ components/client/widget/progressbar/Progressbar.js
@@ -1,12 +1,13 @@
 import { getStrapiMedia } from "lib/media";
 import styles from "./progressbar.module.css";
 import styled from 'styled-components';
+import { Tooltip } from 'antd';
 
 const Progressbar = ({ progressbar }) => {
     console.log(progressbar);
     const Photo = styled.img`
-    width:20px;
-    height:20px;
+    width:25px;
+    height:25px;
     :hover{
         opacity:0.8;
         cursor:pointer;
@@ -14,18 +15,31 @@ const Progressbar = ({ progressbar }) => {
     `
 
     const Link = styled.a`
-        background-color:#006400 ;
+        background-color:${progressbar.progressbarColor} ;
         :after{
-            color: gray ;
+            border-left-color: ${progressbar.progressbarColor} ;
         };
-
     `
-    return progressbar.steps ? <div id={styles.circle}>
-        <ul>
-            {progressbar.steps?.map((step) => {
-                return <li><Link href="#1" ><Photo src={getStrapiMedia(step.icon)}></Photo></Link></li>
-            })}
-        </ul>
+    return progressbar.steps ? <div>
+        {progressbar.progressbarStyle === "circle-mode" || !progressbar.progressbarStyle ?
+            <div id={styles.circle}>
+                <ul>
+                    {progressbar.steps?.map((step) => {
+                        return <li><Link ><Photo src={getStrapiMedia(step.icon)}></Photo></Link></li>
+                    })}
+                </ul>
+            </div> :
+            <div id={styles.flash}>
+                <ul>
+                    {progressbar.steps?.map((step) => {
+                        return <li>
+                            <Link className={step.id <= progressbar.acitveStep ? styles.active : styles.disable}>
+                                <Tooltip title={step.hover_text}><Photo src={getStrapiMedia(step?.icon)}></Photo></Tooltip></Link>
+                        </li>
+                    })}
+                </ul>
+            </div >
+        }
     </div> : null;
 }
 export default Progressbar;
