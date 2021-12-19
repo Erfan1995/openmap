2f93214da2dc8e61df34f4efa260b8cdd42ae03a
madadjo <hab.hussaini@gmail.com>
2021-12-19 22:52:52 +0430

    solved problem

--- components/customer/mapComponents/UserLocationDetails.js
+++ components/customer/mapComponents/UserLocationDetails.js
@@ -43,9 +43,9 @@ const UserLocationDetails = ({ id }) => {
 
     const menu = (
         <Menu >
-            <Menu.Item key="0"><a onClick={() => setVisible(true)}>{DATASET.Unlock}</a></Menu.Item>
+            <Menu.Item key="0"><a onClick={() => setVisible(true)}>Location Details</a></Menu.Item>
             <Menu.Divider />
-            <Menu.Item key="1"><a onClick={() => onGetUserLocation()}>get user location</a></Menu.Item>
+            <Menu.Item key="1"><a onClick={() => onGetUserLocation()}>Get User Location</a></Menu.Item>
         </Menu>
     );
 
